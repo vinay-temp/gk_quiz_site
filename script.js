48b985a9ca4bdeef1a2c9f1a6e77349e0493cf05
vinay-temp <117427777+vinay-temp@users.noreply.github.com>
2024-11-27 01:35:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -65,9 +65,10 @@ function createPage(data) {
 
     if (question_set.length == 0) return;
 
-    document.getElementById("start-page").classList.add("hidden");
-    document.getElementById("quiz-container").classList.remove("hidden");
-    STARTQUIZ(question_set);
+    window.open(
+      `start.html?data=${encodeURIComponent(JSON.stringify(question_set))}`,
+      "_blank"
+    );
   });
 }
 
@@ -81,99 +82,4 @@ fetch("https://gk-server.glitch.me/get_database")
     console.error("Error fetching data:", error);
   });
 
-// !
-// Quiz code starts here
-// !
-
-var score, totalScore, seen, clickCount;
-
-function STARTQUIZ(data) {
-  score = 0;
-  totalScore = data.length;
-  seen = new Set();
-
-  UpdateQuiz(data);
-}
-
-function UpdateQuiz(data) {
-  document.getElementById("msg").style.color = "#121212";
-  document.getElementById("score").innerHTML = `${score}/${totalScore}`;
-  document.getElementById("tips").innerHTML = "";
-  options.innerHTML = "";
-  clickCount = 0;
-
-  if (score == totalScore) {
-    document.getElementById("question").innerHTML = "Completed";
-    document.getElementById("score").innerHTML = `${score}/${totalScore}`;
-    return;
-  }
-
-  let num = Math.floor(Math.random() * totalScore);
-  while (seen.has(num)) {
-    num = Math.floor(Math.random() * totalScore);
-  }
-
-  let q = data[num];
-  document.getElementById("question").innerHTML = q.question;
-  document.getElementById("tips").innerHTML = q.tips;
-  document.getElementById("tips").classList.add("hidden");
-
-  updateOptions(data, q, num);
-}
-
-function updateOptions(data, q, num) {
-  for (let i = 1; i <= 4; i++) {
-    let option = document.createElement("button");
-
-    option.className = "option";
-    option.id = "option" + i;
-
-    option.innerHTML = q[option.id];
-    option.value = q[option.id];
-
-    options.appendChild(option);
-  }
-
-  options.childNodes.forEach((option) => {
-    option.addEventListener("click", () => {
-      document.getElementById("tips").classList.remove("hidden");
-
-      if (q.answer == option.value) {
-        if (clickCount == 0) {
-          score++;
-          seen.add(num);
-        }
-        document.getElementById("score").innerHTML = `${score}/${totalScore}`;
-        option.style.background = "green";
-        clickCount++;
-
-        if (clickCount == 2) {
-          UpdateQuiz(data);
-        } else {
-          document.getElementById("msg").style.color = "white";
-
-          options.childNodes.forEach((btn, i) => {
-            btn.disabled = true;
-
-            if (q.answer == btn.value) {
-              btn.style.background = "green";
-              btn.disabled = false;
-            }
-          });
-        }
-      } else {
-        option.style.background = "#FF1A1A";
-        clickCount++;
-        document.getElementById("msg").style.color = "white";
-        options.childNodes.forEach((btn, i) => {
-          btn.disabled = true;
-
-          if (q.answer == btn.value) {
-            btn.style.background = "green";
-            btn.disabled = false;
-          }
-        });
-      }
-    });
-  });
-}
+console.log("Version: 1");
